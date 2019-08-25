69958009e2d9b5ce24d024b322560c59973fd094
gdarold <gdarold@172.19.1.6>
2019-08-25 11:39:10 -0300

    ifcbot2

--- src/Implementa/Main.java
+++ src/Implementa/Main.java
@@ -17,32 +17,23 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //Criação do objeto bot com as informações de acesso
         TelegramBot bot = TelegramBotAdapter.build("930998203:AAFWhrECZbiVhydGzjTtsr_WchL41_Zg4Ng");
 
-        //objeto responsável por receber as mensagens
         GetUpdatesResponse updatesResponse;
-        //objeto responsável por gerenciar o envio de respostas
+
         SendResponse sendResponse;
-        //objeto responsável por gerenciar o envio de ações do chat
+
         BaseResponse baseResponse;
 
-        //controle de off-set, isto é, a partir deste ID será lido as mensagens pendentes na fila
         int m = 0;
 
-        //loop infinito pode ser alterado por algum timer de intervalo curto
         while (true) {
 
-            //executa comando no Telegram para obter as mensagens pendentes a partir de um off-set (limite inicial)
             updatesResponse = bot.execute(new GetUpdates().limit(100).offset(m));
 
-            //lista de mensagens
             List<Update> updates = updatesResponse.updates();
 
-            //análise de cada ação da mensagem
             for (Update update : updates) {
-
-                //atualização do off-set
                 m = update.updateId() + 1;
 
                 System.out.println("Recebendo mensagem:" + update.message().text());
@@ -72,8 +63,6 @@ public class Main {
                     sendResponse = bot.execute(new SendMessage(update.message()
                             .chat().id(), "Aguarde estamos consultando nosso estoque"));
                 }
-                
-                
 
             }
 
