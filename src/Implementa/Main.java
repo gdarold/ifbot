60871f56df39fffd0848ea1617ac9804d63431b1
gdarold <gdarold@172.19.1.6>
2019-08-25 11:35:07 -0300

    ifcbot2

--- src/Implementa/Main.java
+++ src/Implementa/Main.java
@@ -72,6 +72,8 @@ public class Main {
                     sendResponse = bot.execute(new SendMessage(update.message()
                             .chat().id(), "Aguarde estamos consultando nosso estoque"));
                 }
+                
+                
 
             }
 
